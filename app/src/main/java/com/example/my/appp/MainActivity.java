0ab8794c9e92e690840794e31e5dcd52733b3e40
qq1039423266 <1039423266@qq.com>
2016-06-30 17:53:11 +0800

    111

--- app/src/main/java/com/example/my/appp/MainActivity.java
+++ app/src/main/java/com/example/my/appp/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-Button button;
+public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
